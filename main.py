88c64c6e9f21426711fca3ab222d8830a7568055
the1ninjaking <shazzergamer@gmail.com>
2025-01-21 09:12:24 -0500

    added display and refresh+ removed unneed tracking of files

--- main.py
+++ main.py
@@ -2,9 +2,22 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
-    print("Starting asteroids")
+    pygame.init()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(000)
+        pygame.display.flip()
+    
 
 if __name__ == "__main__":
     main()
