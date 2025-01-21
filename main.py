1780dbdad3cca7c9a630f4e6bf7f5fd9dfc8855b
the1ninjaking <shazzergamer@gmail.com>
2025-01-21 09:25:09 -0500

    capped game fps to 60

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +19,7 @@ def main():
                 return
         screen.fill(000)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
